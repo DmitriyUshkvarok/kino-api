2b86eadcefabd57efed50ad27f81e53aa7da5ebe
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-01-22 22:12:44 +0200

    ...

--- src/main-js/firebase.js
+++ src/main-js/firebase.js
@@ -11,6 +11,7 @@ import {
   sendPasswordResetEmail,
   signOut,
 } from 'firebase/auth';
+import { getDatabase, ref, set } from 'firebase/database';
 
 import { initializeApp } from 'firebase/app';
 
@@ -56,15 +57,17 @@ const firebaseConfig = {
   storageBucket: 'kino-practice.appspot.com',
   messagingSenderId: '564661411790',
   appId: '1:564661411790:web:a813ef026114618e1fd59f',
+  databaseURL:
+    'https://kino-practice-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const authStateChange = getAuth();
 function authState() {
   onAuthStateChanged(authStateChange, user => {
     if (user) {
-      // console.log(user);
       const uid = user.uid;
       userInfoWrapper.innerHTML = `<div class='info-user'>
         <img
@@ -80,6 +83,11 @@ function authState() {
     } else {
       // userInfoWrapper.innerHTML = `<div class='info-user'>
       //   <div class='info-container'>
+      //      <img
+      //     class='info-usrer-photo'
+      //     src='../images/480954349_.thumb.png.d69f3d288fe9b425cdfbb8fd67a77d5f.png'
+      //     alt=''
+      //   />
       //   <h3 class='info-user-name'>гость</h3>
       //   <div class='info-user-email'>нет адреса</div>
       //   </div>
@@ -92,29 +100,38 @@ authState();
 function onResetPasswword(e) {
   e.preventDefault();
   emailForReset.innerHTML = ` <label class="feedback-form__label" for="email">enter the email address you provided during registration</label>
-          <input class="input-email" type="email"
+          <input class="input-emails" type="email"
             name="email"
             id="email"
             autocomplete="on">
               <button class="btn-new-password" type="submit" value=""
               id='submit'>Получить новый пароль</button>`;
   const newPassword = document.querySelector('.btn-new-password');
+  const mailValue = document.querySelector('.input-emails');
   newPassword.addEventListener('click', onSubmitNewPassword);
 }
 
 // сброс пароля
-const authPass = getAuth();
 
-function onSubmitNewPassword(e) {
+const authPass = getAuth();
+authPass.languageCode = 'ru';
+
+async function onSubmitNewPassword(e) {
+  const mailValue = document.querySelector('.input-emails');
   e.preventDefault();
-  sendPasswordResetEmail(authPass, email)
+  const email = mailValue.value;
+  console.log(email);
+
+  await sendPasswordResetEmail(authPass, email)
     .then(() => {
-      Notify.success(`привет ${user} письмо отправлено`);
+      console.log('ecgtiyj');
+      Notify.success(`привет! письмо отправлено`);
     })
     .catch(error => {
-      // const errorCode = error.code;
-      // const errorMessage = error.message;
-      // // ..
+      Notify.failure('ошибка', {
+        timeout: 4000,
+        background: '#c9a22de6',
+      });
     });
 }
 
@@ -134,9 +151,14 @@ function onRegisterEmailAndPhone(e) {
         window.removeEventListener('load', onStopBackground);
         Notify.success('Спасибо за регестрацию');
         localStorage.setItem(TOKEN_KEY, token);
+        writeUserData(
+          user.providerId,
+          user.displayName,
+          user.email,
+          user.photoURL
+        );
       }
       const user = userCredential.user;
-      console.log(user);
     })
     .catch(error => {
       onErrorValid(error);
@@ -175,12 +197,15 @@ const provider = new GoogleAuthProvider();
 function onSignFunctionGoogle() {
   signInWithPopup(auth, provider)
     .then(result => {
+      console.log(result);
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       const user = result.user.displayName;
       Notify.success(`привет ${user}`);
       localStorage.setItem(TOKEN_KEY, token);
+      // writeUserData(user.uid, user.displayName, user.email, user.photoURL);
     })
+    .catch(console.log('error'))
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -265,4 +290,22 @@ function onErrorValid(error) {
     });
   }
   return;
+}
+
+// add user database
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  })
+    .then(() => {
+      console.log('ok');
+      // Data saved successfully!
+    })
+    .catch(error => {
+      console.log('no ok');
+      // The write failed...
+    });
 }
